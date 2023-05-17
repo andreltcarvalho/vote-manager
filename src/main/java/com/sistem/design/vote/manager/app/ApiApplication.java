0cf85c744fc6b8d1277950f501aa0bab3dbb07ea
andreltcarvalho <andreltcarvalho@hotmail.com>
2023-05-17 19:47:05 -0300

    fix(project): Fixed logback configuration to show tomcat + custom logs.

--- src/main/java/com/sistem/design/vote/manager/app/ApiApplication.java
+++ src/main/java/com/sistem/design/vote/manager/app/ApiApplication.java
@@ -1,17 +1,10 @@
 package com.sistem.design.vote.manager.app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ApiApplication {
-
-    Logger logger = LoggerFactory.getLogger(ApiApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
